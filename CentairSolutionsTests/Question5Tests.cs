a2078afeeef179bb33f8ad34127cedfd42fb9ac9
Dan Miller <dsmiller95@gmail.com>
2016-10-24 21:10:46 -0500

    added tests and code for Question 5

---
+++ CentairSolutionsTests/Question5Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentairSolutions;
+
+namespace CentairSolutionsTests
+{
+    [TestClass]
+    public class Question5Tests
+    {
+
+
+        [TestMethod]
+        public void HappyPathDigitizer()
+        {
+            var digitizer = new Digitizer();
+            string initial = "One2Three4";
+            string expeted = "24";
+
+            Assert.IsTrue(expeted.Equals(digitizer.GetDigits(initial)));
+        }
+    }
+}
