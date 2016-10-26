1c48d62d0114d3dac47ba29799ff8f536559ebe3
Dan Miller <dsmiller95@gmail.com>
2016-10-26 10:32:56 -0500

    added question 8

---
+++ CentairSolutionsTests/Question8Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentairSolutions;
+
+namespace CentairSolutionsTests
+{
+    [TestClass]
+    public class Question8DiamondTests
+    {
+
+
+        [TestMethod]
+        public void FilledDiamondTest()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+    *    
+   * *   
+  * * *  
+ * * * * 
+* * * * *
+ * * * * 
+  * * *  
+   * *   
+    *    
+";
+            diamondeer.PrintDiamond(5, true);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+        [TestMethod]
+        public void EmptyDiamondTest()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+    *    
+   * *   
+  *   *  
+ *     * 
+*       *
+ *     * 
+  *   *  
+   * *   
+    *    
+";
+            diamondeer.PrintDiamond(5, false);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+    }
+}
