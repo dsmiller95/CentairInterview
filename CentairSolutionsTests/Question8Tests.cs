e40dca2af30793d3d62b5de1b39dc3c2f49a8c7d
Dan Miller <dsmiller95@gmail.com>
2016-10-26 10:51:28 -0500

    added more tests

--- CentairSolutionsTests/Question8Tests.cs
+++ CentairSolutionsTests/Question8Tests.cs
@@ -13,7 +13,7 @@ namespace CentairSolutionsTests
 
 
         [TestMethod]
-        public void FilledDiamondTest()
+        public void FilledDiamondTest5()
         {
             var myWriter = new StringWriter();
             var diamondeer = new Diamondeer(myWriter);
@@ -52,6 +52,138 @@ namespace CentairSolutionsTests
     *    
 ";
             diamondeer.PrintDiamond(5, false);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+
+        [TestMethod]
+        public void FilledDiamondTest4()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+   *   
+  * *  
+ * * * 
+* * * *
+ * * * 
+  * *  
+   *   
+";
+            diamondeer.PrintDiamond(4, true);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+
+        [TestMethod]
+        public void FilledDiamondTest2()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+ * 
+* *
+ * 
+";
+            diamondeer.PrintDiamond(2, true);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+
+
+        [TestMethod]
+        public void FilledDiamondTest1()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+*
+";
+            diamondeer.PrintDiamond(1, true);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+        [TestMethod]
+        public void FilledDiamondTest0()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+";
+            diamondeer.PrintDiamond(0, true);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+        [TestMethod]
+        public void EmptyDiamondTest4()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+   *   
+  * *  
+ *   * 
+*     *
+ *   * 
+  * *  
+   *   
+";
+            diamondeer.PrintDiamond(4, false);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+        [TestMethod]
+        public void EmptyDiamondTest2()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+ * 
+* *
+ * 
+";
+            diamondeer.PrintDiamond(2, false);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+
+
+        [TestMethod]
+        public void EmptyDiamondTest1()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+*
+";
+            diamondeer.PrintDiamond(1, false);
+            Assert.AreEqual(expected,
+                myWriter.ToString());
+        }
+
+        [TestMethod]
+        public void EmptyDiamondTest0()
+        {
+            var myWriter = new StringWriter();
+            var diamondeer = new Diamondeer(myWriter);
+
+            string expected = @"
+";
+            diamondeer.PrintDiamond(0, false);
             Assert.AreEqual(expected,
                 myWriter.ToString());
         }
