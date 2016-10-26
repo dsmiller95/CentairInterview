4db0ec640c6c728bd856b396f6e1a6ed2061a097
Dan Miller <dsmiller95@gmail.com>
2016-10-26 11:25:07 -0500

    added custom coctail shaker sort

--- CentairSolutionsTests/Question4Tests.cs
+++ CentairSolutionsTests/Question4Tests.cs
@@ -18,18 +18,71 @@ namespace CentairSolutionsTests
             int[] initial = {5, 8, 2, -1, 0};
             int[] expeted = {-1, 0, 2, 5, 8};
 
-            Assert.IsTrue(expeted.SequenceEqual(sorter.SortArray(initial)));
+            var result = sorter.SortArray(initial);
+
+            Assert.IsTrue(expeted.SequenceEqual(result), "Sequence not sorted");
         }
 
 
         [TestMethod]
-        public void HappyPathNiaveSort()
+        public void HappyPathProductionSort()
         {
             var sorter = new Sorter();
             int[] initial = { 5, 8, 2, -1, 0 };
             int[] expeted = { -1, 0, 2, 5, 8 };
 
-            Assert.IsTrue(expeted.SequenceEqual(sorter.NiaveSort(initial)));
+            Assert.IsTrue(expeted.SequenceEqual(sorter.ProductionSort(initial)));
+        }
+
+
+
+        [TestMethod]
+        public void HappyPathSort2()
+        {
+            var sorter = new Sorter();
+            int[] initial = { 5, 4, 3, 2, 1, 0 };
+            int[] expeted = { 0, 1, 2, 3, 4, 5 };
+
+            var result = sorter.SortArray(initial);
+
+            foreach (var i in result) { Console.Out.WriteLine(i); }
+
+            Assert.IsTrue(expeted.SequenceEqual(result), "Sequence not sorted");
+        }
+        [TestMethod]
+        public void HappyPathSort3()
+        {
+            var sorter = new Sorter();
+            int[] initial = { 0, 1, 2, 3, 4, 5 };
+            int[] expeted = { 0, 1, 2, 3, 4, 5 };
+
+            var result = sorter.SortArray(initial);
+
+            foreach (var i in result) { Console.Out.WriteLine(i); }
+
+            Assert.IsTrue(expeted.SequenceEqual(result), "Sequence not sorted");
+        }
+        [TestMethod]
+        public void HappyPathSort4()
+        {
+            var sorter = new Sorter();
+            int[] initial = { 1, 0 };
+            int[] expeted = { 0, 1 };
+
+            var result = sorter.SortArray(initial);
+
+            foreach (var i in result) { Console.Out.WriteLine(i); }
+
+            Assert.IsTrue(expeted.SequenceEqual(result), "Sequence not sorted");
+        }
+
+
+        [TestMethod]
+        public void SadPathSort()
+        {
+            var sorter = new Sorter();
+
+            Assert.IsNull(sorter.SortArray(null));
         }
     }
 }
